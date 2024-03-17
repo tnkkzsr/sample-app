c37e11fb69fe02743781ef420b04f515f3106547
tnkkzsr <107390719+tnkkzsr@users.noreply.github.com>
2024-03-17 13:41:53 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,13 @@ Rails.application.routes.draw do
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
 end
